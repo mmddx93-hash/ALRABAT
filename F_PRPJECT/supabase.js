e4514c3e8ba70ffdbccfeb13057024798f2d289d
mmddx93-hash <mmddx93@gmail.com>
2026-06-30 13:48:44 +0300

    Update supabase.js

--- F_PRPJECT/supabase.js
+++ F_PRPJECT/supabase.js
@@ -1,4 +1,4 @@
-﻿import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-const SUPABASE_URL = "https://rzryyiugvxnvvlafacuk.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ6cnl5aXVndnhudnZsYWZhY3VrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3ODI2NjIsImV4cCI6MjA5NTM1ODY2Mn0.SgQBL1_4yTcA4jBXh5vFPKWXrazSWK-FZE51BGf-XN8";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+const SUPABASE_URL = "https://gyrkrxestxdltxclujhm.supabase.co/rest/v1/";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd5cmtyeGVzdHhkbHR4Y2x1amhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI4MTE4MjAsImV4cCI6MjA5ODM4NzgyMH0.cneO8EciGx6o54h2RkAd9q7YgSppZc9NEPyU_uWOXOo";
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
