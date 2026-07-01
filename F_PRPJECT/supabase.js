d3ac877e5505a522d1dd9e9d2847bd0e12ebf441
mmddx93-hash <mmddx93@gmail.com>
2026-07-01 09:43:19 +0300

    Update supabase.js

--- F_PRPJECT/supabase.js
+++ F_PRPJECT/supabase.js
@@ -1,4 +1,4 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-const SUPABASE_URL = "https://gyrkrxestxdltxclujhm.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://gyrkrxestxdltxclujhm.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd5cmtyeGVzdHhkbHR4Y2x1amhtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI4MTE4MjAsImV4cCI6MjA5ODM4NzgyMH0.cneO8EciGx6o54h2RkAd9q7YgSppZc9NEPyU_uWOXOo";
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
